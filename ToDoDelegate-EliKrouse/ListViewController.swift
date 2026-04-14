d8a56b0cab0a60652a5a2149ad0b0597e0e346b3
Eli-Krouse <williamelijahkrouse@gmail.com>
2026-04-14 12:07:35 -0400

    Initial Commit

---
+++ ToDoDelegate-EliKrouse/ListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ToDoDelegate-EliKrouse
+//
+//  Created by Krouse, William E. on 4/14/26.
+//
+
+import UIKit
+
+class ListViewController: UIViewController
+{
+    var toDoList = [String]()
+    
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ListViewController: UITableViewDataSource, UITableViewDelegate
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
---
+++ ToDoDelegate-EliKrouse/SaveViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SaveViewController.swift
+//  ToDoDelegate-EliKrouse
+//
+//  Created by Krouse, William E. on 4/14/26.
+//
+
+import Foundation
